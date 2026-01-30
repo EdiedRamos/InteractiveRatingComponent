e32d667667bf8378ce26d24c9473c9fd9bcfa331
EdiedRamos <edieisa22@gmail.com>
2026-01-29 22:23:26 -0500

    feat: complete new structure and styles

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-// get reference to star options
-const starOptions = document.querySelector(".stars-container").children;
+document.addEventListener('DOMContentLoaded', () => {
+  const firstContainer = document.getElementById('first-container');
+  const secondContainer = document.getElementById('second-container');
 
-// submit button
-const submitButton = document.getElementById("submit");
+  firstContainer.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-// save the last star selected
-let before = -1;
+    const formData = new FormData(firstContainer);
 
-// add active class and remove the last added
-for (let i = 0; i < starOptions.length; i++) {
-  starOptions[i].addEventListener("click", () => {
-    if (~before) {
-      starOptions[before].classList.remove("active");
+    const cardTag = document.getElementById('card-tag');
+    if (cardTag) {
+      const message = `You selected ${formData.get('stars')} out of 5`;
+      cardTag.innerText = message;
     }
-    starOptions[i].classList.add("active");
-    before = i;
-  });
-}
 
-// show the result container and stars selected
-submitButton.addEventListener("click", () => {
-  if (~before) {
-    // make the first container hidden
-    document.getElementById("first-container").classList.add("hide");
-    // make the second container visible
-    document.getElementById("second-container").classList.remove("hide");
-    // put stars selected on the second container paragraph
-    document.getElementById(
-      "stars-selected"
-    ).children[0].innerText = `You selected ${before + 1} out of 5`;
-  } else {
-    alert("choose an option");
-  }
+    firstContainer.classList.add('is-hidden');
+    secondContainer.classList.remove('is-hidden');
+  });
 });
